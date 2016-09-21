233327c3ba7587887466c79a208925b7e16a7908
Luke Noble <luke.noble@primarysite.net>
2016-09-21 10:37:36 +0100

    Adding file

---
+++ WhatBoardGame/GameDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameDetailsViewController.swift
+//  WhatBoardGame
+//
+//  Created by Luke Noble on 20/09/2016.
+//  Copyright © 2016 Luke Noble. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GameDetailsViewController : UIViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+deinit {
+    NotificationCenter.default.removeObserver(self)
+}
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    nameLabel.text = "Let's scan a game!"
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(setLabels(_:)), name: NSNotification.Name(rawValue: "NameNotification"), object: nil)
+}
+
+func setLabels(_ notification: Notification){
+    
+    // Use the data from DataService.swift to initialize the Album.
+    let gameInfo = game(name: DataService.dataService.NAME_FROM_OUTPAN)
+    nameLabel.text = "\(gameInfo.name)"
+}
+}
